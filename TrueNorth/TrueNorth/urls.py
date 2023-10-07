30602e951f9919d4678ae5dae39f424091259059
winter_x64 <75623356+winter-x64@users.noreply.github.com>
2023-10-08 00:05:38 +0530

    ❄️ add files
    add the file and update files

--- TrueNorth/TrueNorth/urls.py
+++ TrueNorth/TrueNorth/urls.py
@@ -4,6 +4,7 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("Home.urls")),
+    path("explore/", include("maps.urls")),
     # path("auth/", include("AuthUser.urls")),
     # path("chat/", include("ChatApp.urls")),
     path("user/", include("users.urls")),
--- TrueNorth/maps/views.py
+++ TrueNorth/maps/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landingpage(request):
+    return render(request, "explore/landing_page.html")
